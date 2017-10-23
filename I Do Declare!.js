109baa74673442a0271f7f998d420f46c61b72c1
Mis0791 <sturgill.michelle91@comcast.net>
2017-10-23 00:09:48 -0700

    Create I Do Declare!.js

---
+++ I Do Declare!.js
@@ -0,0 +1,27 @@
+var firstName = "Michelle";
+console.log("My name is " + firstName);
+var numDays = 365;
+console.log("There are " + numDays + " in a year.")
+var numWeeks = 14;
+console.log("I will be coding for " + numWeeks + " weeks at Coding Dojo!")
+
+var sum = 30 + -17;
+console.log(sum);
+var sum = 100 * 35;
+console.log(sum);
+var sum = 763 - 27;
+console.log(sum);
+var sum = 456 / 56;
+console.log(sum);
+
+var hikingisFun = true;
+console.log(hikingisFun);
+var icecreamisBad = false;
+console.log(icecreamisBad);
+var x;
+
+if (x === undefined) {
+  txt = "x is undefined";
+} else {
+  txt = "x is defined";
+}

7982b93456d5a09dc2cecef95c0936f121bb7f24
Mis0791 <sturgill.michelle91@comcast.net>
2017-10-30 11:51:12 -0700

    Add files via upload

---
+++ filterbytype.py
@@ -0,0 +1,20 @@
+#integer
+for i in range(45, 455):
+    if(i >= 100):
+        print "That's a big number!"
+    if(i < 100):
+        print "That's a small number."
+
+#string
+for i in range(1, 50):
+    if(i >= 50):
+        print "Long sentence."
+    if(i < 50):
+        print "Short sentence."
+        
+#list
+for i in range(0, 10):
+    if(i >= 10):
+        print "Big list!"
+    if(i > 10):
+        print "Short list."

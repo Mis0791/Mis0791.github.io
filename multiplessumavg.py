1f4277d6187171f830fa02ab72488afb38b8585a
Mis0791 <sturgill.michelle91@comcast.net>
2017-10-30 10:53:03 -0700

    Add files via upload

---
+++ multiplessumavg.py
@@ -0,0 +1,17 @@
+#print odds
+for i in range(1, 1000, 2):
+    print(i)
+
+#multiples of 5 to 1,000,000
+for i in range(5, 1000000, 5):
+    print(i)
+
+#print sum list
+a = [1, 2, 5, 10, 255, 3]
+print a
+print sum(a)
+
+#print avg
+a = [1, 2, 5, 10, 255, 3]
+print a
+print sum(a)/len(a)

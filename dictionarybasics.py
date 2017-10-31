a9ac87efc7a756c59015f173e40b8523cc512f1b
Mis0791 <sturgill.michelle91@comcast.net>
2017-10-30 21:43:55 -0700

    Add files via upload

---
+++ dictionarybasics.py
@@ -0,0 +1,11 @@
+def dictionbasics():
+      
+  Myself = {}
+  Myself["name"] = "My name is Michelle"
+  Myself["age"] = "My age is 26"
+  Myself["country"] = "My country of birth is the United States"
+  Myself["language"] = "My favorite language is Python"
+
+  for key, value in Myself.interitems() :
+      print key, value
+

14221fba30acfd43c3bd83e8701f4f0c1bfa41f5
Mis0791 <sturgill.michelle91@comcast.net>
2017-11-16 17:48:58 -0800

    Add files via upload

---
+++ login_reg/views.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
+
+def index(request):
+    return render(request, 'loginreg/index.html')
+
+def register(request):
+    result = User.objects.validate_registration(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully logged in!")
+    return redirect('/books')
+
+def login(request):
+    result = User.objects.validate_login(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully logged in!")
+    return redirect('/success') 
+
+def success(request):
+    try:
+        request.session['user_id']
+    except KeyError:
+        return redirect('/')
+    return render(request, 'loginreg/success.html', { "user": User.objects.get(id=request.session['user_id'])})
